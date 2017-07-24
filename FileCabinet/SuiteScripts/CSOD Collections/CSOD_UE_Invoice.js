0acd2dbb107f401560ca3f2e75a9c52da1b8c749
Chan Yi <huichanyi@gmail.com>
2017-07-24 09:01:35 -0700

    Initial Push

---
+++ FileCabinet/SuiteScripts/CSOD Collections/CSOD_UE_Invoice.js
@@ -0,0 +1,65 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType UserEventScript
+ * @NModuleScope SameAccount
+ */
+define(['N/record'],
+/**
+ * @param {record} record
+ */
+function(record) {
+   
+    /**
+     * Function definition to be triggered before record is loaded.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.newRecord - New record
+     * @param {string} scriptContext.type - Trigger type
+     * @param {Form} scriptContext.form - Current form
+     * @Since 2015.2
+     */
+    function beforeLoad(scriptContext) {
+    	
+    	
+    }
+
+    /**
+     * Function definition to be triggered before record is loaded.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.newRecord - New record
+     * @param {Record} scriptContext.oldRecord - Old record
+     * @param {string} scriptContext.type - Trigger type
+     * @Since 2015.2
+     */
+    function beforeSubmit(scriptContext) {
+    	var record = scriptContext.newRecord;
+    	var origDueDate = record.getValue({ fieldId: 'duedate' });
+    	var invoiceSentDate = record.getValue({ fieldId: 'custbody_record_emailed_date' });
+    	
+    	log.debug({
+    		title: 'Date fields validation',
+    		details: 'Due Date : ' + origDueDate + ', ' + 'Invoice Sent Date: ' + invoiceSentDate
+    	});
+    }
+
+    /**
+     * Function definition to be triggered before record is loaded.
+     *
+     * @param {Object} scriptContext
+     * @param {Record} scriptContext.newRecord - New record
+     * @param {Record} scriptContext.oldRecord - Old record
+     * @param {string} scriptContext.type - Trigger type
+     * @Since 2015.2
+     */
+    function afterSubmit(scriptContext) {
+
+    }
+
+    return {
+        //beforeLoad: beforeLoad,
+        beforeSubmit: beforeSubmit
+        //afterSubmit: afterSubmit
+    };
+    
+});
