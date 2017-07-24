227e71681d2efe4c1bcfd9d0cc14adac5ab40da8
Chan Yi <huichanyi@gmail.com>
2017-07-24 16:07:15 -0700

    Added more validation

--- FileCabinet/SuiteScripts/CSOD Collections/Libraries/CSOD_UE_Invoice_Utils.js
+++ FileCabinet/SuiteScripts/CSOD Collections/Libraries/CSOD_UE_Invoice_Utils.js
@@ -9,41 +9,23 @@ function(moment, format) {
 	
 	var exports = {};
 	
-	// function to update custbody_adjusted_due_date
-	// X = custbody_record_emailed_date - origDueDate
-	// if X <= -15 then origDueDate
-	// if X > -15 then custbody_record_emailed_date + 14 days
-	
-	function updateAdjustDueDate(context) {
-		var record = context.newRecord;
-		var origDueDate = moment(record.getValue({ fieldId: 'duedate' }));
-		var invoiceSentDate = moment(record.getValue({ fieldId: 'custbody_record_emailed_date' }));
-		var dateDiff = invoiceSentDate.diff(origDueDate, 'days'); // type is 'number'
-		var adjustedDueDate;
+	function CalcDate(startDate) {
 		
-		if(dateDiff > -15) {
-			adjustedDueDate = invoiceSentDate.add(14, 'days');
-		} else {
-			adjustedDueDate = origDueDate;
+		var that = this;
+		
+		this.getMomentDate = function() {
+			startDate = moment(startDate);
+			return that; 
+		};
+		
+		this.getDiff = function(compareDate) {
+			compareDate = moment(compareDate);
+			return startDate.diff(compareDate, 'days');
 		}
-		
-		var dateToWrite = getFormattedDate(adjustedDueDate);
-		
-		log.debug({
-			title: 'Date fields validation',
-			details: 'Due Date : ' + origDueDate + ', ' + 'Invoice Sent Date: ' + invoiceSentDate + ', Date Diff: ' + dateDiff
-		});
-		
-		log.debug({
-			title: 'Date to write',
-			details: dateToWrite
-		});
-		
-		
-		
 	}
 	
 	function getFormattedDate(dateToFormat) {
+		
 		var parsedDate = format.parse({
 			value: dateToFormat,
 			type: format.Type.DATE
@@ -55,6 +37,49 @@ function(moment, format) {
 		});
 		
 	}
+	
+	
+	// function to update custbody_adjusted_due_date
+	// X = custbody_record_emailed_date - origDueDate
+	// if X <= -15 then origDueDate
+	// if X > -15 then custbody_record_emailed_date + 14 days
+	
+	function updateAdjustDueDate(context) {
+		var record = context.newRecord;
+		var origDueDate = record.getValue({ fieldId: 'duedate' });
+		var invoiceSentDate = record.getValue({ fieldId: 'custbody_record_emailed_date' });
+		
+		if(invoiceSentDate && origDueDate) {
+			var dateDiff = new CalcDate(invoiceSentDate).getMomentDate().getDiff(origDueDate);
+			
+			var adjustedDueDate;
+			
+			if(dateDiff > -15) {
+				adjustedDueDate = getFormattedDate(moment(invoiceSentDate).add(14, 'days'));
+			} else {
+				adjustedDueDate = getFormattedDate(moment(origDueDate));
+			}
+			
+			log.debug({
+				title: 'Date fields validation',
+				details: 'Due Date : ' + origDueDate + ', ' + 'Invoice Sent Date: ' + invoiceSentDate + ', Date Diff: ' + dateDiff
+			});
+			
+			log.debug({
+				title: 'Date to write',
+				details: adjustedDueDate + ', Type : ' + typeof adjustedDueDate + ', ' + new Date(adjustedDueDate)
+			});
+			
+			record.setValue({
+				fieldId: 'custbody_adjusted_due_date',
+				value: new Date(adjustedDueDate),
+				ignoreFieldChange : true
+			});
+		}
+
+	}
+	
+
 
 	exports.updateAdjustDueDate = updateAdjustDueDate;
     return exports;
